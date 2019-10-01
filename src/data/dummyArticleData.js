fd5cb4624cb0d3b353a5818a72c2c64ce38b36d3
Beardedflea <Linzell.Godfrey@live.co.uk>
2019-10-01 12:20:06 +0100

    Added News Element and dummyArticleData

---
+++ src/data/dummyArticleData.js
@@ -0,0 +1,31 @@
+let articles = [
+    {
+        title: "New news",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa ultricies mi quis hendrerit. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Dolor magna eget est lorem ipsum dolor sit. Cursus in hac habitasse platea dictumst. Volutpat lacus laoreet non curabitur. Aenean vel elit scelerisque mauris. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Lorem donec massa sapien faucibus et molestie ac. Nulla pellentesque dignissim enim sit amet venenatis.",
+        author:"Beardedflea",
+        visible: true,
+    },
+    {
+        title: "Older news",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa ultricies mi quis hendrerit. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Dolor magna eget est lorem ipsum dolor sit. Cursus in hac habitasse platea dictumst. Volutpat lacus laoreet non curabitur. Aenean vel elit scelerisque mauris. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Lorem donec massa sapien faucibus et molestie ac. Nulla pellentesque dignissim enim sit amet venenatis.",
+        author:"Beardedflea",
+        visible: true,
+    },
+    {
+        title: "Oldest News",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa ultricies mi quis hendrerit. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Dolor magna eget est lorem ipsum dolor sit. Cursus in hac habitasse platea dictumst. Volutpat lacus laoreet non curabitur. Aenean vel elit scelerisque mauris. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Lorem donec massa sapien faucibus et molestie ac. Nulla pellentesque dignissim enim sit amet venenatis.",
+        author:"Beardedflea",
+        visible: true,
+    },
+]
+
+module.exports ={
+    articles
+}
+
+// {
+//     title: "Article Title",
+//     content: "Paragraph stuff",
+//     author:"who wrote it",
+//     visible: whether to display the article or not, boolean,
+// }
