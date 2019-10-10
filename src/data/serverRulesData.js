bc0b79d4400f0650e290a190cf8da6cd1bcf09a4
Beardedflea <Linzell.Godfrey@live.co.uk>
2019-10-10 01:49:23 +0100

    Adjusted rules conversion to work with infinity and possibly all 1.7.10 servers

--- src/data/serverRulesData.js
+++ src/data/serverRulesData.js
@@ -1,27 +1,34 @@
 
-const fsp = require('fs').promises;
+const fs = require('fs')
 
-let rulesJSON;
-const colourCodeCheck = /&[0-9A-z]/g
-const ruleNoCheck = /[1-9]?[0-9][.]/g
+const URfilePath = './URrules.txt';
 
-const filePath = './URrules.txt';
-const promise = fsp.readFile(filePath, { encoding: 'utf8' });
+const IEfilePath = './IErules.txt';
 
-promise.then(data => convertRulesToJSON(data))
-.then(rules => fsp.writeFile('URrules.js', rules, 'utf8', function(){
+const rulesData = fs.readFileSync(URfilePath, { encoding: 'utf8' });
+
+const rulesObjArray = convertRulesToJSON(rulesData)
+
+fs.writeFileSync('IErules.json', rulesObjArray, 'utf8', function(){
     if(error) throw error;
-}))
+    console.log('successfully converted to JSON format')
+})
 
 function convertRulesToJSON(txt){
-    let dataArray = txt.split('\n').map(str => str.replace(colourCodeCheck, ''))
-    
-    let rulesArray = dataArray.filter(rule => rule.match(ruleNoCheck))
+    let rulesJSON;
+    const colourCodeCheck = /&[0-9A-z]/g
+    const ruleNoCheck = /[1-9]?[0-9][.]/g
 
+    let dataArray = txt.split('\n').map(str => str.replace(colourCodeCheck, ''))
+    let rulesArray = dataArray.filter(rule => rule.match(ruleNoCheck))
+ 
     rulesJSON = rulesArray.map(line =>{
         let dotIndex = line.indexOf('.')
         let ruleObj = {number: line.slice(0, dotIndex+1), desc: line.slice(dotIndex+2)}
+        if(ruleObj.desc.includes('\r')){
+            ruleObj.desc = ruleObj.desc.slice(0,-1)
+        }
         return ruleObj
      })
-    return rulesJSON
+    return JSON.stringify(rulesJSON)
 }
