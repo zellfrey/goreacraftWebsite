d100233d61edd8fa3de2beecf31ea1650959cb78
Beardedflea <Linzell.Godfrey@live.co.uk>
2019-10-30 13:29:09 +0000

    Created new file system allowing for easier management of server data

--- src/data/MCServerData/testData/serverDataConversion.js
+++ src/data/MCServerData/testData/serverDataConversion.js
@@ -1,12 +1,13 @@
 const CLI_TXT = require ('../converterTextHelp.js');
 const SERVER_LIST = require('../serverNavData.js').ftbServers
 const fs = require('fs')
+const yaml = require('js-yaml');
 const path = require('path');
 const ServerDataFldr = "../testData/";
 
 let serverDirList
 
-
+let serverDataJSON = []
 
 function searchServerFolder(){
     serverDirList =  fs.readdirSync(ServerDataFldr)
@@ -14,23 +15,46 @@ function searchServerFolder(){
 
     serverDirList.map(dir =>console.log(`Found ${dir} directory, adding to list`))
 
-    return scanEachServer(serverDirList)
+    return serverDirList
 };
 
 function scanEachServer(directoryList){
     directoryList.forEach(function(dir){
-        let directoryObj = {modPack: dir, files: null}
+        let directoryObj = {server: dir, files: null}
 
         directoryObj.files = fs.readdirSync(dir).filter((file) => fs.lstatSync(path.join(dir, file)).isFile());
 
-        console.log(directoryObj)
+        serverDataJSON.push(directoryObj)
+        
     })
+    return serverDataJSON
 }
 
+function loadFilesFromServerDirectory(serverData){
+    serverData.forEach(function(dir){
+
+            dir.rules = dir.files.includes('rules.txt') ? 
+                        fs.readFileSync(`./${dir.server}/` +'rules.txt', {encoding: 'utf-8'})
+                        : null
+
+            dir.bannedItems = dir.files.includes('Restrict List.yml') ? 
+                        yaml.safeLoad(fs.readFileSync(`./${dir.server}/Restrict List.yml`, {encoding: 'utf-8'})) 
+                        : null
+
+            dir.serverStaff = dir.files.includes('users.yml') ? yaml.safeLoad(fs.readFileSync(`./${dir.server}/users.yml`, {encoding: 'utf-8'})) 
+                        : null
+    })
+    return serverData
+}
+
+
 searchServerFolder()
+scanEachServer(serverDirList)
+loadFilesFromServerDirectory(serverDataJSON)
 
 let serverDirObjArray =[
     // {modPack: null,staffList: {},banneditems:[],rules:[],},
     // {modPack: null,staffList: {},banneditems:[],rules:[],},
     // {modPack: null,staffList: {},banneditems:[],rules:[],},
 ]
+
